9047c6fcdfbcde9157b583b38bdd576c52b1de32
ToxichQQQ <storozhenko.dev@gmail.com>
2022-01-19 23:36:24 +0300

    Life: added game page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {StartPage} from "./components/StartPage";
 import {Game} from "./components/Game";
 
@@ -6,11 +6,118 @@ import styles from './main.module.css'
 
 
 function App() {
-  return (
-    <div className={styles.root}>
-        <StartPage/>
-    </div>
-  );
+    const [genetation, setGeneration] = useState(1)
+    const [cells, setCells] = useState([])
+    const [intervalId, setIntervalId] = useState(null)
+    const fieldSize = 10
+
+    const handleGenerateCells = () => {
+        let finnalArray = []
+        for (let i = 0; i < fieldSize; i++) {
+            let newArray = []
+            for (let i = 0; i < fieldSize; i++) {
+                newArray.push({isAlive: false})
+            }
+            finnalArray.push(newArray)
+        }
+        return finnalArray
+    }
+
+    const handleCellClick = (rowIndex, cellIndex, value) => {
+        setCells(prevState => prevState.map((row, index) => index === rowIndex ? row.map((cell, cIndex) => cellIndex === cIndex ? {isAlive: value} : cell) : row))
+    }
+
+    const handleRestartGame = () => {
+        setCells(handleGenerateCells())
+        clearInterval(intervalId)
+        setGeneration(1)
+    }
+
+    const handleSetAlive = (rowIndex, cellIndex) => {
+        if (rowIndex < 0 || cellIndex < 0 || rowIndex > fieldSize - 1 || cellIndex > fieldSize - 1) {
+            return false
+        }
+        if (cells[rowIndex][cellIndex].isAlive) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    const handleAliveCell = (rowIndex, cellIndex) => {
+        let aliveCells = 0
+        if (handleSetAlive(rowIndex, cellIndex - 1)) {
+            aliveCells = aliveCells + 1
+        }
+        if (handleSetAlive(rowIndex, cellIndex + 1)) {
+            aliveCells = aliveCells + 1
+        }
+        if (handleSetAlive(rowIndex + 1, cellIndex)) {
+            aliveCells = aliveCells + 1
+        }
+        if (handleSetAlive(rowIndex - 1, cellIndex)) {
+            aliveCells = aliveCells + 1
+        }
+        if (handleSetAlive(rowIndex + 1, cellIndex - 1)) {
+            aliveCells = aliveCells + 1
+        }
+        if (handleSetAlive(rowIndex + 1, cellIndex + 1)) {
+            aliveCells = aliveCells + 1
+        }
+        if (handleSetAlive(rowIndex - 1, cellIndex + 1)) {
+            aliveCells = aliveCells + 1
+        }
+        if (handleSetAlive(rowIndex - 1, cellIndex - 1)) {
+            aliveCells = aliveCells + 1
+        }
+
+        if (aliveCells === 3) {
+            handleCellClick(rowIndex, cellIndex, true)
+        } else {
+            handleCellClick(rowIndex, cellIndex, false)
+        }
+
+    }
+
+    const handleCreateNewGenerate = () => {
+        for (let i = 0; i < fieldSize; i++) {
+            for (let k = 0; k < fieldSize; k++) {
+                handleAliveCell(i, k)
+            }
+        }
+        setGeneration(prevState => prevState + 1)
+    }
+
+
+    useEffect(() => {
+        setCells(handleGenerateCells())
+    }, [])
+
+    const startGame = () => {
+        handleCreateNewGenerate()
+    }
+
+    const finishGame = () => {
+        return cells.every(row => row.every(cell => cell.isAlive === false))
+    }
+
+    const pauseGame = () => {
+        clearInterval(intervalId)
+    }
+
+    useEffect(() => {
+        if (finishGame()) {
+            setGeneration(1)
+            clearInterval(intervalId)
+        }
+    }, [cells])
+
+    return (
+        <div className={styles.root}>
+            <Game pauseGame={pauseGame} genetation={genetation} cells={cells} finishGame={finishGame} startGame={startGame} handleCellClick={handleCellClick}
+                  handleRestartGame={handleRestartGame} handleSetAlive={handleSetAlive}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {makeStyles} from "@mui/styles";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import pauseButtonBg from '../assests/img/pauseIcon.png'
+import restartButtonBg from '../assests/img/restartIcon.png'
+import playButtonBg from '../assests/img/playIcon.png'
+
+const useStyles = makeStyles(() => ({
+    gameContainer: {
+        paddingTop: 10
+    },
+    gameField: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        borderRadius: 4,
+        padding: 0,
+        width: '500px !important',
+        height: '500px !important',
+        border: '5px solid  rgba(23, 178, 222, 0.24)',
+        boxSizing: 'content-box !important',
+        backgroundImage: 'linear-gradient(90.9deg, rgba(247, 58, 245, 0.24) 3.28%, rgba(23, 178, 222, 0.24) 97.89%) !important'
+    },
+    generationInfo: {
+        textAlign: 'center',
+    },
+    generationText: {
+        color: '#E5EAF9',
+        letterSpacing: '6px !important',
+        fontFamily: 'Poiret One, cursive !important',
+        fontSize: '36px !important',
+        padding: '12px 138px !important',
+        backgroundColor: '#1b3a56',
+        borderRadius: '19px',
+        display: 'inline-block !important',
+        width: '290px !important',
+        height: '45px !important',
+    },
+    gameButton: {
+        marginRight: '60px !important',
+        marginLeft: '60px !important',
+        boxShadow: '6px 7px 22px 0px #00000059',
+        width: '89px !important',
+        height: '89px !important',
+        borderRadius: 23,
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat',
+        backgroundBlendMode: 'multiply',
+        backgroundColor: 'linear-gradient(140.98deg, rgba(247, 58, 245, 0.15) 8.49%, rgba(23, 178, 222, 0.25) 100%) !important',
+    },
+    restartButton: {
+        backgroundImage: `url(${restartButtonBg})`,
+    },
+    pauseButton: {
+        backgroundImage: `url(${pauseButtonBg})`,
+    },
+    playButton: {
+        backgroundImage: `url(${playButtonBg})`,
+    },
+    cell: {
+        width: 50,
+        height: 50,
+        border: '1px solid black'
+    }
+}))
+
+export function Game({handleCellClick, startGame, pauseGame, handleRestartGame,genetation,cells}) {
+    const classes = useStyles()
 
 
-export function Game(){
-    return(
-        <></>
+    return (
+        <Grid container justifyContent='center'>
+            <Grid item xs={8} className={classes.gameContainer}>
+                <Grid container justifyContent='center'>
+                    <Grid item className={classes.gameField}>
+                        {cells.map((row, rowIndex) => row.map((cell, cellIndex) => <Grid item key={cellIndex}
+                                                                                         onClick={() => handleCellClick(rowIndex, cellIndex, true)}
+                                                                                         className={classes.cell}
+                                                                                         style={{backgroundColor: cell.isAlive ? '#E5EAF9' : 'transparent'}}></Grid>))}
+                    </Grid>
+                    <Grid item xs={12} className={classes.generationInfo}>
+                        <p className={classes.generationText}>Generation:{genetation}</p>
+                    </Grid>
+                    <Grid item>
+                        <Button className={`${classes.gameButton} ${classes.pauseButton}`}
+                                onClick={pauseGame}></Button>
+                        <Button className={`${classes.gameButton} ${classes.playButton}`}
+                                onClick={startGame}></Button>
+                        <Button className={`${classes.gameButton} ${classes.restartButton}`}
+                                onClick={handleRestartGame}></Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
